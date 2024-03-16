f2d44f4673a1d14336881f23b68fedcd60747130
Shobhit kumar parashar <shobhitparashar0@gmail.com>
2024-03-16 16:43:53 +0530

    update Controller

--- route/product.js
+++ route/product.js
@@ -1,62 +1,17 @@
 const routes = require('express').Router();
-const { Router } = require('express');
 const { Product, ProductCategory } = require('../model/product');
 const User = require('../model/user');
+const { createProduct, getProducts, updateProduct, deleteProduct } = require('../controller/productController');
 
-routes.post('/', async (req, res) => {
-	// const user = req.user._id;
-	// const owner = User.findById(user);
-	const { name, description, price, productCategory, owner } = req.body;
-	try {
-		const product = new Product({
-			name,
-			description,
-			price,
-			productCategory,
-			owner // here should be the user object id
-		});
-		const savedProduct = await product.save();
-		res.json(savedProduct);
-	} catch (error) {
-		res.json({ message: error });
-	}
-});
+routes.post('/', createProduct);
 
-routes.get('/:id', async (req, res) => {
-	try {
-		const products = await Product.findById(req.params.id);
-		res.json(products);
-	} catch (error) {
-		res.json({ message: error });
-	}
-});
+routes.get('/:id', getProducts);
 
 // update route
-routes.patch('/:id', async (req, res) => {
-	try {
-		const updatedProduct = await Product.findById(req.params.id);
-		updatedProduct.name = req.body.name;
-		updatedProduct.description = req.body.description;
-		updatedProduct.price = req.body.price;
-		updatedProduct.productCategory = req.body.productCategory;
-		updatedProduct.owner = req.body.owner;
-		const savedProduct = await updatedProduct.save();
-
-		res.json(savedProduct);
-	} catch (error) {
-		res.json({ message: error });
-	}
-});
+routes.patch('/:id', updateProduct);
 
 // delete route
 
-routes.delete('/:id', async (req, res) => {
-	try {
-		const deletedProduct = await Product.findByIdAndDelete(req.params.id);
-		res.json(deletedProduct);
-	} catch (error) {
-		res.json({ message: error });
-	}
-});
+routes.delete('/:id', deleteProduct);
 
 module.exports = routes;
